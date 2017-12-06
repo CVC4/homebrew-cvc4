e029dfcd33c4e426e0463a863cc1ec04e27fda24
Juan Chanco <juan.chanco@bluestembrands.com>
2017-12-06 11:28:05 -0600

    new sha256 for https://cvc4.cs.stanford.edu/downloads/builds/src/cvc4-1.5.tar.gz

--- Formula/cvc4.rb
+++ Formula/cvc4.rb
@@ -2,7 +2,7 @@ class Cvc4 < Formula
   desc "Open-source automatic theorem prover for SMT"
   homepage "https://cvc4.cs.stanford.edu/"
   url "https://cvc4.cs.stanford.edu/downloads/builds/src/cvc4-1.5.tar.gz"
-  sha256 "11ee2c3c182556a5ef750da9631191b3ad6c4ea592eb81d067a2edc41e57bd7b"
+  sha256 "5d6b4f8ee8420f85e3f804181341cedf6ea32342c48f355a5be87754152b14e9"
 
   depends_on "boost" => :build
   depends_on "gmp"
