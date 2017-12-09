25360e36ad281f9145149a8d0086277f28bba790
Andres Noetzli <andres.noetzli@gmail.com>
2017-12-09 02:55:53 -0800

    Add support for Java bindings

--- Formula/cvc4.rb
+++ Formula/cvc4.rb
@@ -4,10 +4,14 @@ class Cvc4 < Formula
   url "https://cvc4.cs.stanford.edu/downloads/builds/src/cvc4-1.5.tar.gz"
   sha256 "5d6b4f8ee8420f85e3f804181341cedf6ea32342c48f355a5be87754152b14e9"
 
+  option "with-java-bindings", "Compile with Java bindings"
+
   depends_on "boost" => :build
   depends_on "gmp"
   depends_on "libantlr3c"
   depends_on "antlr@3" => :build
+  depends_on :java if build.with? "java-bindings"
+  depends_on "swig@2" => :build if build.with? "java-bindings"
   depends_on :arch => :x86_64
 
   def install
@@ -18,6 +22,14 @@ class Cvc4 < Formula
             "--with-gmp",
             "--with-antlr-dir=#{Formula["libantlr3c"].opt_prefix}",
             "--prefix=#{prefix}"]
+
+
+    if build.with? "java-bindings"
+      args << "--enable-language-bindings=java"
+      args << "CFLAGS=-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/"
+      args << "CXXFLAGS=-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/"
+    end
+
     system "./configure", *args
     system "make", "install"
   end
