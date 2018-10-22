3de010c4ec5081b0dcaa14dbda0693a8fa8939c3
Andres Noetzli <andres.noetzli@gmail.com>
2018-10-22 14:51:47 -0700

    Add CryptoMiniSat support

---
+++ Formula/cryptominisat.rb
@@ -0,0 +1,16 @@
+class Cryptominisat < Formula
+  desc "An advanced SAT solver"
+  homepage "https://www.msoos.org/"
+  url "https://github.com/msoos/cryptominisat/archive/5.6.5.tar.gz"
+  sha256 "b2751f8380a59c4885bea4c297536f0af2230306b1458d3e6b78d6e29f37b9d2"
+
+  depends_on "cmake" => :build
+  depends_on :arch => :x86_64
+
+  def install
+    mkdir "build" do
+      system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".."
+      system "make", "install"
+    end
+  end
+end
--- Formula/cvc4.rb
+++ Formula/cvc4.rb
@@ -18,6 +18,7 @@ class Cvc4 < Formula
   depends_on "swig"
   depends_on "automake" => :build if not build.head?
   depends_on "libtool" => :build if not build.head?
+  depends_on "cryptominisat" => :recommended
   depends_on :arch => :x86_64
 
   def install
@@ -26,6 +27,7 @@ class Cvc4 < Formula
 
     if build.head?
       args = ["--prefix=#{prefix}",
+              "--cryptominisat",
               "--symfpu"]
 
       if build.with? "java-bindings"
@@ -52,7 +54,8 @@ class Cvc4 < Formula
               allow_gpl? ? "--enable-gpl" : "--bsd",
               "--with-gmp",
               "--prefix=#{prefix}",
-              "--with-symfpu"]
+              "--with-symfpu",
+              "--with-cryptominisat"]
 
       if build.with? "java-bindings"
         args << "--enable-language-bindings=java"
