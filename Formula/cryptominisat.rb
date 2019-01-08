07cdb317e998960ce5b38d70694f0b18e05749fa
Andres Noetzli <andres.noetzli@gmail.com>
2019-01-07 17:33:31 -0800

    Update to CryptoMiniSat 5.6.5

--- Formula/cryptominisat.rb
+++ Formula/cryptominisat.rb
@@ -1,8 +1,8 @@
 class Cryptominisat < Formula
   desc "An advanced SAT solver"
   homepage "https://www.msoos.org/"
-  url "https://github.com/msoos/cryptominisat/archive/5.6.5.tar.gz"
-  sha256 "b2751f8380a59c4885bea4c297536f0af2230306b1458d3e6b78d6e29f37b9d2"
+  url "https://github.com/msoos/cryptominisat/archive/5.6.6.tar.gz"
+  sha256 "95fac3df4311d4fb6e2674b1bce3113056795a2eda51cd807f73bcc4f9b1a2d5"
 
   depends_on "boost" => :recommended
   depends_on "cmake" => :build
